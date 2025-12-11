33c8e7bda89cc32df3fc538d1851691e65a167e7
Raafat205 <rofa2511@gmail.com>
2025-12-11 19:00:17 +0200

    final initial client isa

---
+++ client/src/app/core/models/user.interface.ts
@@ -0,0 +1,27 @@
+export interface UserSignUp {
+    full_name: string;
+    email: string;
+    password: string;
+}
+
+export interface UserLogin {
+    email: string;
+    password: string;
+}
+
+export interface SignUpResponse {
+    success:boolean;
+    message:string;
+}
+
+export interface LoginResponse {
+    success:boolean;
+    message:string;
+    user: User;
+}
+export interface User  {
+        id:number;
+        name: string;
+        email: string;
+        role: string;
+    }
