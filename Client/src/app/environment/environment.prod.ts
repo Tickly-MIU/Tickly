5a25261f929237e9ecd069907eb1ded8d43fe25a
Ahmedmo-27 <ahmed2303479@miuegypt.edu.eg>
2025-12-19 05:27:46 +0200

    Update production environment configuration to use new backend API base URL

--- Client/src/app/environment/environment.prod.ts
+++ Client/src/app/environment/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   /**
    * Base URL of the backend API for production.
    * Frontend: https://tickly-3f3fb62f8bf7.herokuapp.com/
-   * Backend: https://tickly.page.gd/server
+   * Backend: https://tickly-backend-a247ddfb7eba.herokuapp.com/
    */
-  API_BASE: 'https://tickly.page.gd/Server/public/api'
+  API_BASE: 'https://tickly-backend-a247ddfb7eba.herokuapp.com/api'
 };
