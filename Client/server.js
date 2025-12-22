e5d22a6fffc65f8f391d51136b485f8b79f8a824
Ahmedmo-27 <ahmed2303479@miuegypt.edu.eg>
2025-12-23 00:23:32 +0200

    Update server port to 4200 and enable session check in HomeComponent

--- Client/server.js
+++ Client/server.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/tickly/browser/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4200;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
